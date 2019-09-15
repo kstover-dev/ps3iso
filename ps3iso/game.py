9d4f4451a9963b9ed4f8739941a1cbc5bb9adb28
Kira Stover <dev@kstover.net>
2019-09-15 21:03:11 +1000

    Move rename function into Game class

--- ps3iso/game.py
+++ ps3iso/game.py
@@ -1,30 +1,36 @@
 import io
 import re
+import glob
 import subprocess
 from pathlib import Path
+from collections import Counter
 
 from .sfo import SfoFile
 
 
 class Game(object):
+
     def __init__(self, iso_path):
         self.iso = Path(iso_path).resolve()
-        self.files = set(self.iso.parent.glob(self.iso.stem + '.*'))
+        self.files = set(self.iso.parent.glob(glob.escape(self.iso.stem) + '.*'))
         self.sfo = self.extract_sfo(self.iso)
 
-    def format(self, f, fmt, fill=''):
-        name = str(fmt
-                    .replace('%C', self.sfo.CATEGORY.strip())
-                    .replace('%P', str(self.sfo.PARENTAL_LEVEL).strip())
-                    .replace('%p', self.sfo.PS3_SYSTEM_VER.strip())
-                    .replace('%R', str(self.sfo.RESOLUTION).strip())
-                    .replace('%S', str(self.sfo.SOUND_FORMAT).strip())
-                    .replace('%T', self.sfo.TITLE.strip())
-                    .replace('%I', self.sfo.TITLE_ID.strip())
-                    .replace('%V', self.sfo.VERSION.strip()))
-
+    def format_file(self, f, fmt, fill=''):
+        name = self.sfo.format(fmt)
         name = re.sub(r'[\\/*?:<>"|%]', fill, name)
         return (f.parent / name).with_suffix(f.suffix.lower())
+
+    def print_info(self, fmt=None):
+        if fmt is not None:
+            for f in self.files:
+                print(self.sfo.format(fmt)
+                      .replace('\\n', '\n')
+                      .replace('\\t', '\t')
+                      .replace('%F', str(f)))
+        else:
+            width = max(len(str(k)) for k, v in self.sfo)
+            print(f'\n{self.iso}')
+            print('\n'.join(f'\t{k.ljust(width)}: {v}' for k, v in self.sfo))
 
     def __repr__(self):
         return f'<{self.iso}|+{len(self.files) - 1}>'
@@ -46,3 +52,27 @@ class Game(object):
                 yield cls(fpath)
         else:
             yield cls(path)
+
+    @staticmethod
+    def rename_all(games, fmt):
+        # Create a list of (src, dst) tuples
+        targets = set((f, game.format_file(f, fmt)) for game in games for f in game.files)
+        # Remove duplicates
+        counter = Counter(t[1] for t in targets)
+        duplicates = set(t for t in targets if counter[t[1]] != 1)
+        targets -= duplicates
+
+        def maxwidth(_targets):
+            return max(len(str(t[0])) for t in _targets)
+
+        width = maxwidth(targets)
+        for src, dst in sorted(targets, key=lambda x: x[0]):
+            print(f'{str(src).ljust(width)} -> {dst}')
+            src.rename(dst)
+
+        if duplicates:
+            print('\nCowardly refusing to rename files which result in duplicates being overwritten:')
+            width = maxwidth(duplicates)
+            for src, dst in sorted(duplicates, key=lambda x: x[1]):
+                print(f'\t{str(src).ljust(width)} -> {dst}')
+
--- ps3iso/info.py
+++
@@ -1,7 +0,0 @@
-from .game import Game
-
-def info():
-    pass
-
-def info_all(path):
-    pass
