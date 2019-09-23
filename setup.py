ca9a993ba669b6250ef75d2d464edc06ec42903a
Kira Stover <dev@kstover.net>
2019-09-23 20:14:47 +1000

    Update version number

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md') as f:
 
 setup(
     name='ps3iso',
-    version='0.3.3',
+    version='0.3.4',
     url='https://git.sr.ht/~jmstover/ps3iso',
     license='MIT',
     author='Joshua Stover',
