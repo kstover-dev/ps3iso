ea12da64fd4f4cf566af6847a81d8d6736e9ef76
Kira Stover <dev@kstover.net>
2019-11-06 20:51:21 +1100

    Updated SFO package with edit and write support.
    	* SFO Package
    
    	Added a dedicated package for more robust handling of SFO files.
    	This adds edit and write support to SFO files, but introduces some
    	changes to the basic SfoFile interface.
    
    
    	* SFO Parsing
    
    	Handing of SFO file sections has been split into separate classes.
    	Each class implements the __bytes__ method, including `SfoFile`,
    	and makes better use of the `struct` library to parse each section.
    
    
    	* SFO Parameters
    
    	SFO Parameters are now accessible via SfoFile.parameters property.
    	This is a NamedTuple containing SFO Parameters as attributes, so
    	should be usable in place of the old SfoFile objects.
    
    	Parameters can be modified by with the new SfoFile methods:
    		get/set/remove_parameter()
    
    	An SfoFile object can be checked for all required parameters by 
    	passing an SfoCategory enum item into SfoFile.verify_parameters
    	This allows checking for valid SFO files for various systems.
    
    	Parameters with variable-key names are now supported
    	(e.g. TITLE_02, TITLE_33, PARENTAL_LEVEL_K etc.)
    
    
    	* SFO Parameter Table
    
    	Added a dictionary of all valid SFO parameters:
    		ps3iso.sfo.parameters.VALID_SFO_PARAMETERS
    
    	Parameters implement a `copy()` method, which means this dict can
    	be used to create new valid parameters.
    	SfoParameter.new() creates new SfoParameter objects from the table
    	based on key name.
    	The table encodes data from https://psdevwiki.com/ps3/PARAM.SFO
    
    
    	* Named Exceptions
    
    	Added a bunch of named exceptions for better error handling.
    	See `ps3iso.sfo.errors`
    
    
    	* Testing
    
    	Updated tests for ps3iso.sfo package.
    	Added tests for ps3iso.game
    	Added doctest extension.

--- test/__main__.py
+++ test/__main__.py
@@ -1,3 +1,31 @@
-import pytest
+import sys
+from pytest import main
+from pathlib import Path
+
+from .test_sfo_file import TestSfo
+
+
+tmp_files = (
+    Path(TestSfo.OUTFILE),
+    Path('NewTitle.SFO'),
+)
+
+pytest_args = [
+    '--color=yes',
+    '--doctest-modules',
+    '--doctest-glob=README.*',
+]
+
+
+def cleanup():
+    for path in tmp_files:
+        try:
+            path.unlink()
+        except FileNotFoundError:
+            pass
+
+
 if __name__ == '__main__':
-    exit(pytest.main())
+    returncode = main(pytest_args)
+    cleanup()
+    exit(returncode)
